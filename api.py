3541253f67652dd40d7d8fc6f7b2bb1b9d35acb2
james-prodigy <james.burgess@prodigyfinance.com>
2020-12-26 10:18:51 +0200

    update db location
    Signed-off-by: james-prodigy <james.burgess@prodigyfinance.com>

--- api.py
+++ api.py
@@ -140,7 +140,7 @@ def spot():
     return "Invalid request"
 
 if __name__ == '__main__':
-    db = TinyDB('db.json')
+    db = TinyDB('./db/db.json')
 
     if admin := getenv("ADMIN_USER"):
         if not db.table('users').search(Query().email == admin):
